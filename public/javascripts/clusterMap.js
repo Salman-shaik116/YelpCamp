234b82179e8ba18214a6568bf68cfee3c8a3e634
Salman <salman39302@gmail.com>
2025-09-19 01:49:22 +0530

    added the clustering for maps

---
+++ public/javascripts/clusterMap.js
@@ -0,0 +1,113 @@
+maptilersdk.config.apiKey = maptilerApiKey;
+
+const map = new maptilersdk.Map({
+    container: 'map',
+    style: maptilersdk.MapStyle.BRIGHT,
+    center: [-103.59179687498357, 40.66995747013945],
+    zoom: 3
+});
+
+map.on('load', function () {
+    map.addSource('campgrounds', {
+        type: 'geojson',
+        data: campgrounds,
+        cluster: true,
+        clusterMaxZoom: 14, // Max zoom to cluster points on
+        clusterRadius: 50 // Radius of each cluster when clustering points (defaults to 50)
+    });
+
+    map.addLayer({
+        id: 'clusters',
+        type: 'circle',
+        source: 'campgrounds',
+        filter: ['has', 'point_count'],
+        paint: {
+            // Use step expressions (https://docs.maptiler.com/gl-style-specification/expressions/#step)
+            // with three steps to implement three types of circles:
+            'circle-color': [
+                'step',
+                ['get', 'point_count'],
+                '#51bbd6',   // 0-9: blue
+                10, '#f1f075',   // 10-29: yellow
+                30, '#f28cb1',   // 30-89: pink
+                90, '#ff7f50',   // 90-179: coral
+                180, '#8e44ad'   // 180+: vibrant purple
+            ],
+            'circle-radius': [
+                'step',
+                ['get', 'point_count'],
+                15,  // 0-9
+                10, 20,  // 10-29
+                30, 25,  // 30-89
+                90, 30,  // 90-179
+                180, 35  // 180+
+            ]
+        }
+    });
+
+    map.addLayer({
+        id: 'cluster-count',
+        type: 'symbol',
+        source: 'campgrounds',
+        filter: ['has', 'point_count'],
+        layout: {
+            'text-field': '{point_count_abbreviated}',
+            'text-font': ['DIN Offc Pro Medium', 'Arial Unicode MS Bold'],
+            'text-size': 12
+        }
+    });
+
+    map.addLayer({
+        id: 'unclustered-point',
+        type: 'circle',
+        source: 'campgrounds',
+        filter: ['!', ['has', 'point_count']],
+        paint: {
+            'circle-color': '#11b4da',
+            'circle-radius': 4,
+            'circle-stroke-width': 1,
+            'circle-stroke-color': '#fff'
+        }
+    });
+
+    // inspect a cluster on click
+    map.on('click', 'clusters', async (e) => {
+        const features = map.queryRenderedFeatures(e.point, {
+            layers: ['clusters']
+        });
+        const clusterId = features[0].properties.cluster_id;
+        const zoom = await map.getSource('campgrounds').getClusterExpansionZoom(clusterId);
+        map.easeTo({
+            center: features[0].geometry.coordinates,
+            zoom
+        });
+    });
+
+    // When a click event occurs on a feature in
+    // the unclustered-point layer, open a popup at
+    // the location of the feature, with
+    // description HTML from its properties.
+    map.on('click', 'unclustered-point', function (e) {
+        const { popUpMarkup } = e.features[0].properties;
+        const coordinates = e.features[0].geometry.coordinates.slice();
+
+        // Ensure that if the map is zoomed out such that
+        // multiple copies of the feature are visible, the
+        // popup appears over the copy being pointed to.
+        while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+            coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+        }
+
+        new maptilersdk.Popup()
+            .setLngLat(coordinates)
+            .setHTML(popUpMarkup)
+            .addTo(map);
+    });
+
+    map.on('mouseenter', 'clusters', () => {
+        map.getCanvas().style.cursor = 'pointer';
+    });
+    map.on('mouseleave', 'clusters', () => {
+        map.getCanvas().style.cursor = '';
+    });
+});
