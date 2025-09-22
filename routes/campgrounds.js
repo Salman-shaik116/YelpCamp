725e5fac23f331ceab581804b1b701ae1eba7a1c
Salman <salman39302@gmail.com>
2025-09-22 22:09:42 +0530

    refactored campground route

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -13,7 +13,6 @@ router.route('/')
     .get(catchAsync(campgrounds.index))
     .post(isLoggedIn, upload.array('image'), validateCampground, catchAsync(campgrounds.createCampground))
 
-
 router.get('/new', isLoggedIn, campgrounds.renderNewForm)
 
 router.route('/:id')
